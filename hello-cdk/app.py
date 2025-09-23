c80997be931b374537b4fd6900a0d35f6acd5aa0
Constantin Bugneac <constantin.bugneac@bjss.com>
2025-09-23 14:41:17 +0100

    fix typo

--- hello-cdk/app.py
+++ hello-cdk/app.py
@@ -8,7 +8,7 @@ from hello_cdk.db_stack import DatabaseStack
 
 app = cdk.App()
 
-# Usage: cdk deploy -c stack_prefix="feat/123"
+# Usage: cdk deploy -c stack_prefix="feat-123"
 stack_prefix = app.node.try_get_context("stack_prefix") or ""
 
 db_stack = DatabaseStack(app, stack_prefix + "DatabaseStack")
