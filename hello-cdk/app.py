7db4fa13e04bcaccd77d1b7fc38755a7ffe96f4e
Constantin Bugneac <constantin.bugneac@bjss.com>
2025-09-26 16:00:34 +0100

    feat: Add network stack

--- hello-cdk/app.py
+++ hello-cdk/app.py
@@ -5,6 +5,7 @@ import os
 import aws_cdk as cdk
 
 from hello_cdk.db_stack import DatabaseStack
+from hello_cdk.network_stack import NetworkStack
 
 app = cdk.App()
 
@@ -12,5 +13,6 @@ app = cdk.App()
 stack_prefix = app.node.try_get_context("stack_prefix") or ""
 
 db_stack = DatabaseStack(app, stack_prefix + "DatabaseStack")
+network_stack = NetworkStack(app, stack_prefix + "NetworkStack")
 
 app.synth()
---
+++ hello-cdk/hello_cdk/network_stack.py
@@ -0,0 +1,64 @@
+from aws_cdk import (
+    Stack,
+    RemovalPolicy,
+    aws_ec2 as ec2
+)
+
+from constructs import Construct
+
+class NetworkStack(Stack):
+    VPC_ID = "Vpc" # Logical ID
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        vpc_cidr = self.node.try_get_context("vpc_cdr") or "10.0.0.0/16"
+
+        # Create VPC
+        vpc = ec2.Vpc(
+            self,
+            id = self.VPC_ID,
+            vpc_name = f"{id}-{self.VPC_ID}",
+            max_azs = 2,
+            ip_addresses = ec2.IpAddresses.cidr(vpc_cidr),
+            ip_protocol = ec2.IpProtocol.IPV4_ONLY,
+            default_instance_tenancy = ec2.DefaultInstanceTenancy.DEFAULT,
+            create_internet_gateway = True,
+            enable_dns_hostnames = True,
+            enable_dns_support = True,
+            nat_gateway_provider = ec2.NatGatewayProvider.gateway(),
+            nat_gateways = 0,
+            restrict_default_security_group = False,
+            vpn_gateway = False,
+            subnet_configuration = [
+                # Public Subnets
+                ec2.SubnetConfiguration(
+                    name = "public",
+                    subnet_type = ec2.SubnetType.PUBLIC,
+                    cidr_mask = 24,
+                    map_public_ip_on_launch = True,
+                ),
+                # Private Subnets (App)
+                ec2.SubnetConfiguration(
+                    name = "private-app",
+                    subnet_type = ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidr_mask = 24,
+                ),
+                # Private Subnets (DB)
+                ec2.SubnetConfiguration(
+                    name = "private-db",
+                    subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidr_mask = 24,
+                ),
+            ],
+            gateway_endpoints = {
+                "S3": ec2.GatewayVpcEndpointOptions(
+                    service = ec2.GatewayVpcEndpointAwsService.S3
+                ),
+                "DynamoDb": ec2.GatewayVpcEndpointOptions(
+                    service = ec2.GatewayVpcEndpointAwsService.DYNAMODB
+                )
+            }
+        )
+
+        vpc.apply_removal_policy(RemovalPolicy.DESTROY)
