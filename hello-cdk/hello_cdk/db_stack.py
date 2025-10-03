ac6072cdd03ca83d7748a513cfb38cc9ea1632a9
Constantin Bugneac <constantin.bugneac@bjss.com>
2025-10-03 10:41:15 +0100

    fix: Address checkov issues

--- hello-cdk/hello_cdk/db_stack.py
+++ hello-cdk/hello_cdk/db_stack.py
@@ -21,39 +21,41 @@ class DatabaseStack(Stack):
         # Create a new DynamoDB table
         dynamodb_table = dynamodb.Table(
             self,
-            id=self.TABLE_ID,
+            id = self.TABLE_ID,
             table_name = f"{id}-{self.TABLE_ID}",
             partition_key = dynamodb.Attribute(
                 name = "id",
                 type = dynamodb.AttributeType.STRING
             ),
-            sort_key=dynamodb.Attribute(
-                name="timestamp",
-                type=dynamodb.AttributeType.STRING
+            sort_key = dynamodb.Attribute(
+                name = "timestamp",
+                type = dynamodb.AttributeType.STRING
             ),
             table_class = dynamodb.TableClass.STANDARD,
-            time_to_live_attribute="expireAt",
-            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            # read_capacity=5,
-            # write_capacity=5,
+            time_to_live_attribute = "expireAt",
+            billing_mode = dynamodb.BillingMode.PAY_PER_REQUEST,
+            # read_capacity = 5,
+            # write_capacity = 5,
             encryption = dynamodb.TableEncryption.AWS_MANAGED,
-            point_in_time_recovery = True,
+            point_in_time_recovery_specification = dynamodb.PointInTimeRecoverySpecification(
+                point_in_time_recovery_enabled = True
+            ),
             deletion_protection = False,            # NOT recommended for production code
             removal_policy = RemovalPolicy.DESTROY  # NOT recommended for production code
         )
 
         dynamodb_table.add_global_secondary_index(
-            index_name="global-secondary-index",
-            partition_key=dynamodb.Attribute(
-                name="gsipk",
-                type=dynamodb.AttributeType.STRING
+            index_name = "global-secondary-index",
+            partition_key = dynamodb.Attribute(
+                name = "gsipk",
+                type = dynamodb.AttributeType.STRING
             ),
-            sort_key=dynamodb.Attribute(
-                name="gsisk",
-                type=dynamodb.AttributeType.STRING
+            sort_key = dynamodb.Attribute(
+                name = "gsisk",
+                type = dynamodb.AttributeType.STRING
             ),
-            projection_type=dynamodb.ProjectionType.ALL,
+            projection_type = dynamodb.ProjectionType.ALL,
         )
 
         # Output table ARN
-        CfnOutput(self, id="myTableName", value=dynamodb_table.table_name, export_name=f"{id}-myTableName")
+        CfnOutput(self, id = "myTableName", value = dynamodb_table.table_name, export_name = f"{id}-myTableName")
