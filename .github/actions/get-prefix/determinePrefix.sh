8217fb78a93b61c3443678f511a2313211f2461f
Constantin Bugneac <constantin.bugneac@bjss.com>
2025-10-03 10:24:20 +0100

    chore: Make branch prefix shorter for dependabot PRs

--- .github/actions/get-prefix/determinePrefix.sh
+++ .github/actions/get-prefix/determinePrefix.sh
@@ -5,6 +5,11 @@ set -e
 # Get the branch name, stripping out refs/heads/
 BRANCH_NAME=$(echo "$1" | sed 's/refs\/heads\///')
 
+# Make for CDK dependabot prefix shorter to avoid resources name lenght issues
+if [[ "$BRANCH_NAME" == dependabot* ]]; then
+  BRANCH_NAME="${BRANCH_NAME##*/}" # Keep only last part after /
+fi
+
 # Compute stack name from branch
 # 1) replace non [a-z0-9-] (including /) with -
 # 2) trim leading/trailing -
@@ -13,11 +18,6 @@ BRANCH_NAME=$(echo "$1" | sed 's/refs\/heads\///')
 BRANCH_NAME="$(echo "$BRANCH_NAME" \
   | sed -E 's/[^a-z0-9-]+/-/g; s/^-+//; s/-+$//; s/-{2,}/-/g' \
   | cut -c1-127)"
-
-# Make for CDK dependabot prefix shorter to avoid resources name lenght issues
-if [[ "$BRANCH_NAME" == dependabot* ]]; then
-  BRANCH_NAME="${BRANCH_NAME##*/}" # Keep only last part after /
-fi
 
 echo "Branch name: $BRANCH_NAME"
 
